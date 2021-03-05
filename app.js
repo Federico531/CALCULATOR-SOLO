1ecf319e445ef584091d4944999b63bd1e4c1f30
unknown <federicomarchetta@yahoo.com.ar>
2021-03-05 12:29:20 -0300

    pressing a button renders in display

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //TESTS
 //WHEN I PRESS 1 IT SHOWS ON DISPLAY
+const display = document.getElementById('display')
+document.getElementById('1').addEventListener('click', ()=>{
+    display.value =+ "1"
+});
 
-document
