517fc3f0725a31aa483a54cba0dd1924636c1ad8
unknown <federicomarchetta@yahoo.com.ar>
2021-03-06 16:14:09 -0300

    Operation in switch arithmetic method from classes

--- app.js
+++ app.js
@@ -3,30 +3,79 @@ var isOn = false
 var currentValue;
 var values = [];
 var total = 0;
+var symbol= []; 
+
+class Operation {
+    sum(){
+        currentValue = display.value
+        values.push(parseInt(currentValue))
+        display.value = ""
+        currentValue = 0;
+        total += values[values.length - 1];
+        display.value = total;
+    }
+    substract(){
+    }
+    multiply(){
+    }
+    divide(){
+    }
+    equials(){
+
+    }
+}
 
 //ARITHMETICS
 document.getElementById('+').addEventListener('click', () => {
-
-    currentValue = display.value
-    values.push(parseInt(currentValue))
-    display.value = ""
-    currentValue = 0;
-    total += values[values.length - 1];
-    display.value = total;
+const operation = new Operation();
+operation.sum();
 })
 
+document.getElementById('=').addEventListener('click', () => {
+    var lastSymbol = symbol[symbol.length - 1 ]; 
+    var sums = lastSymbol.includes("+");
+    var substracts = lastSymbol.includes("-");
+    var multiplies = lastSymbol.includes("X");
+    var divides = lastSymbol.includes("/");
+    var percentage = lastSymbol.includes("%");
+    
+    switch (true) {
+        case sums:alert('es una suma!');
+        break;
+        case substracts:alert('es una resta!');
+        break;
+        case multiplies:alert('es una multiplicación!');
+        break;
+        case divides:alert('es una division!');
+        break;
+        case percentage:alert('es un porcentaje!');
+        break;
+        default:
+            alert('no es un simbolo valido');
+    }
+    
+    if(includes){
+    alert('funca')
+}
+    //children, case + - / x  --> execute class method
+
+    //ELABORAR ESTE REFACTORING
+    //PODRIA HACER SWITCH CASES ACORDE A UNA CONDICION Y EN ESE CASO EJECUTA LA FUNCION QUE DA DETERMINADO TOTAL?
+    display.value = total;
+})
 document.getElementById('-')
 document.getElementById('/')
 document.getElementById('X')
-document.getElementById('')
+
 
 //NUMBER DISPLAY
 
 document.getElementById('calculator').addEventListener('click', (e) => {
     digit = e.target.childNodes[0].nodeValue
-    digit = parseInt(digit);
+    console.log(digit)
     if (isOn) {
         if (!isNaN(digit)) {
+            digit = parseInt(digit);
             if (display.value === "0") {
                 display.value = ""
                 display.value += digit
@@ -38,6 +87,9 @@ document.getElementById('calculator').addEventListener('click', (e) => {
                     display.value += digit
                 }
             }
+        }else{ //QUE NO HAGA PUSH DE LOS SIMBOLOS DIFERENTES A +, -, /, X, %
+               //SINO VA A CONFUDIR LOS SWITCH Y ME APARECE NaN en pantalla
+           symbol.push(digit);
         }
     }
 })
@@ -56,6 +108,7 @@ document.getElementById("OFF").addEventListener('click', () => {
     isOn = false;
     display.value = ""
     if (!isOn) {
+        //MAKE TOAST 
         console.log("apagado")
     }
 })
