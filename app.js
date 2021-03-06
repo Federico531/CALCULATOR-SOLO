c5d02530711870effb59e25824607e511f9a61b6
unknown <federicomarchetta@yahoo.com.ar>
2021-03-06 08:15:42 -0300

    ON/OFF WORKING

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
-
 const display = document.getElementById('display')
+var isOn = false
+var total;
+
 //var arr = [];
+
+//ARITHMETIC SYMBOLS
+document.getElementById('+').addEventListener('click', () => {
+    
+})
+
+document.getElementById('-')
+document.getElementById('/')
+document.getElementById('X')
+document.getElementById('')
+
+//NUMBER DISPLAY
 
 document.getElementById('calculator').addEventListener('click', (e) => {
     children = e.target.childNodes[0].nodeValue
     children = parseInt(children);
-    if (!isNaN(children)){
-        display.value += children
+    if (isOn) {
+        if (!isNaN(children)) {
+            if (display.value == "0") {
+                display.value = ""
+                display.value += children
+                total = display.value
+            } else {
+                display.value += children
+                total = display.value
+            }
+        }
     }
 })
 
-
 document.getElementById("on/c").addEventListener('click', () => {
+    isOn = true;
+    display.value = "0"
+    if (isOn) {
+        console.log("prendido")
+    }
+})
+
+document.getElementById("OFF").addEventListener('click', () => {
+    isOn = false;
     display.value = ""
+    if (!isOn) {
+        console.log("apagado")
+    }
 })
