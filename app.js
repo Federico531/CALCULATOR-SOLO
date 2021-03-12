397360eb5ab41b5373e394d4f62c50526696de9e
unknown <federicomarchetta@yahoo.com.ar>
2021-03-11 22:03:57 -0300

    varios cambios pocos push, muy mal

--- app.js
+++ app.js
@@ -4,15 +4,61 @@ var currentValue;
 var values = [];
 var total = 0;
 var symbol = [];
-var count = 0;
+var repeatsSymbol = false;
+var lastSymbol = symbol[symbol.length - 1];
+var anyDigit = [];
+
+var numA;
+var numB;
+var operatore;
+
+//SI EL ANTEULTIMO SIMBOLO EXISTE Y ES IGUAL AL ULTIMO SIMBOLO, DEJAR TODO IGUAL
+//SI ES DIFERENTE, Y EXISTEN MAS DE DOS SIMBOLOS EN SIMBOLO, ASIGNAR EL SIMBOLO A OPERATOR
+/*
+    if(symbol[symbol.length - 2]symbol[symbol.length - 2] == lastSymbol){
+    //HACER NADA
+}
+*/
+function operator(e) {
+    operatore = e;
+    return operatore
+}
+//BOCETO DE OPERATION
+function operation(a, operator, b) {
+    if (!operator && !b) {
+        numA = a;
+    } else if (!b) {
+        var simbolo = operator
+    } else if (a & operator & b) {
+        var numB = b;
+    }
+    if (operator == "+") {
+        return a * b
+    }
+}
 class Operation {
+
     sum() {
-        currentValue = display.value
-        values.push(parseInt(currentValue))
-        display.value = ""
-        currentValue = 0;
-        total += values[values.length - 1];
-        display.value = total;
+
+        //BOCETO DEL CONDITIONAL
+        if (numA) {
+            operatore = "+" //crear variable operator
+            operation(numA, operatore) //cuando queremos ejecutar la funcion
+            currentValue = display.value
+            values.push(parseInt(currentValue))
+            display.value = total;
+            currentValue = 0;
+            total += values[values.length - 1]; //ESTO DELEGARLO a operation()
+            display.value = total;
+        } else if (operator == "+") {
+            numB = numA;
+            numA = "";//DO NOTHING
+        }
+        //si toco 2 veces + hace display.value += total (por que?) 
+        //Una calculadora normal si toco 2 veces + a la segunda no hace nada
+        //HACER ESTO SI ENTRA EN EL IF
+
+
     }
     substract() {
     }
@@ -20,7 +66,7 @@ class Operation {
     }
     divide() {
     }
-    equials() {
+    equals() {
 
     }
 }
@@ -28,7 +74,12 @@ class Operation {
 //ARITHMETICS
 document.getElementById('+').addEventListener('click', () => {
     const operation = new Operation();
-    operation.sum();
+    if (repeatsSymbol) {
+
+    } else {
+        operation.sum();
+    }
+
 })
 document.getElementById('-')
 document.getElementById('/')
@@ -37,37 +88,38 @@ document.getElementById('%')
 
 document.getElementById('=').addEventListener('click', () => {
     const operation = new Operation();
-    var lastSymbol = symbol[symbol.length - 1];
+
+    //NO REGISTRA BIEN SI EL ULTIMO SIMBOLO SE REPITIO, Y SIEMPRE SALTA AL SWITCH
+
+
+    console.log('inside the switch LASTsymbol' + symbol[symbol.length - 1]);
 
     switch (true) {
-        case lastSymbol.includes("+"): operation.sum();
-
+        case symbol[symbol.length - 1].includes("+"): operation.sum();
             break;
-        case lastSymbol.includes("-"): alert('es una resta!');
+        case symbol[symbol.length - 1].includes("-"): alert('es una resta!');
             break;
-        case lastSymbol.includes("X"): alert('es una multiplicación!');
+        case symbol[symbol.length - 1].includes("X"): alert('es una multiplicación!');
             break;
-        case lastSymbol.includes("/"): alert('es una division!');
+        case symbol[symbol.length - 1].includes("/"): alert('es una division!');
             break;
-        case lastSymbol.includes("%"): alert('es un porcentaje!');
+        case symbol[symbol.length - 1].includes("%"): alert('es un porcentaje!');
             break;
-        default:
-            alert('no es un simbolo valido');
+        case symbol[symbol.length - 1].includes("="): alert('es un igual!');
+            break;
+        default: alert('no es un simbolo valido');
     }
     display.value = total;
+
 })
-
-
-
 
 //NUMBER DISPLAY
 
 document.getElementById('calculator').addEventListener('click', (e) => {
     var digit = e.target.childNodes[0].nodeValue
-    //ME ESTA DEVOLVIENDO QUE CON ES DIGIT Y QUE "" ES DIGIT
-    console.log(digit)
     if (isOn) {
-        if (isNumber(digit) && !isSymbol(digit) && count != 1) {
+        if (isNumber(digit)) {
+            numA = digit
             console.log("isNumber")
             digit = parseInt(digit);
             if (display.value === "0") {
@@ -82,41 +134,47 @@ document.getElementById('calculator').addEventListener('click', (e) => {
                 }
             }
         } else if (isSymbol(digit)) {
-            symbol.push(digit);
-            console.log(digit)
-            console.log("isSymbol")
+            //LASTSYMBOL DEVUELVE UNDEFINED POR ESO USO LA VARIABLE COMPLETA
+            if (digit == symbol[symbol.length - 1] && !digit.includes("=")) {
+                console.log('ultimo simbolo igual')
+                repeatsSymbol = true;
+                console.log(repeatsSymbol)
+                //if(repeatsSymbol) { NO se EJECUTAN LAS OPERACIONES}
+                //DEBERIA LIMPIAR symbols = [] SI EL OPERADOR ES DIFERENTE
+            } else if (digit !== symbol[symbol.length - 1] || digit.includes("=")) {
+                console.log("no se repite el ultimo");
+                repeatsSymbol = false;
+                symbol.push(digit);
+                console.log(digit)
+                console.log("isSymbol")
+            } else if(digit.includes("=")){
+                
+            }
         } else {
-            console.log("NO ES NADA")
+            console.log("NO ES UN BOTÓN")
         }
     }
 })
 
 function isNumber(digit) {
-    const zero = digit.includes('0')
-    const one = digit.includes('1')
-    const two = digit.includes('2')
-    const three = digit.includes('3')
-    const four = digit.includes('4')
-    const five = digit.includes('5')
-    const six = digit.includes('6')
-    const seven = digit.includes('7')
-    const eight = digit.includes('8')
-    const nine = digit.includes('9')
-    if (zero || one || two || three || four || five || six || seven || eight || nine) {
-        return true
-    } else {
-        return false
+    for (var i = 0; i < 10; i++) {
+        var number = digit.includes(i.toString());
+        if (!number) {
+        } else {
+            return true
+        }
     }
 }
 
 function isSymbol(digit) {
-    var sums = digit.includes("+");
-    var substracts = digit.includes("-");
-    var multiplies = digit.includes("X");
-    var divides = digit.includes("/");
-    var percentages = digit.includes("%");
 
-    if (sums || substracts || multiplies || divides || percentages) {
+    const sums = digit.includes("+");
+    const substracts = digit.includes("-");
+    const multiplies = digit.includes("X");
+    const divides = digit.includes("/");
+    const percentages = digit.includes("%");
+    const equals = digit.includes("=");
+    if (sums || substracts || multiplies || divides || percentages || equals) {
         return true;
     } else {
         return false;
@@ -137,7 +195,23 @@ document.getElementById("OFF").addEventListener('click', () => {
     isOn = false;
     display.value = ""
     if (!isOn) {
-        //MAKE TOAST 
+        //MAKE TOAST SI NO PRENDE EN 10 SEGUNDOS O HACEN CLICK CON LA CALCU APAGADA
         console.log("apagado")
     }
 })
+
+
+//CODIGO DE PRUEBA (BOCETOS)
+//SI REPITE SIMBOLO NO HACER NADA Y REASIGNAR SIMBOLO PARA QUE??
+/*
+if (isSymbol(digit) || isNumber(digit)) {
+    if (lastSymbol == beforeLastSymbol) {
+        //DO NOTHING
+    } else {
+        operator = lastSymbol
+        //si es diferente va a asignar la operacion
+        //deberia esperar un segundo numero para ejecutar resultado
+    }
+}
+
+*/
